a664980c0fa57f8eaf632ed7b820e2f5c5bf3cb4
takumimzd <retnuh.takumi@gmail.com>
2022-06-05 18:44:59 +0900

    controllerの作成とルーティング

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
+  namespace 'api' do
+    namespace 'v1' do
+      resources :rooms, only: %i(show create)
+      resources :themes, only: %i(show create update)
+      resources :themes, only: %i(create)
+    end
+  end
 end
